cba8a6f347674c10cce3574582be81d6d55dbc79
Riley <rstarnes9@gmail.com>
2016-03-18 08:35:04 -0700

    add api call to grab repo names and desc

--- js/main-interface.js
+++ js/main-interface.js
@@ -7,4 +7,11 @@ $(document).ready(function(){
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
+
+  $.get('https://api.github.com/users/daneden/repos?' + apiKey).then(function(response){
+    console.log(response);
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+
 });
