cdcc90284bef0600e43f1058a038bc992fa54d73
Riley <rstarnes9@gmail.com>
2016-03-18 09:45:46 -0700

    add repo names and descs as lists to page

--- js/main-interface.js
+++ js/main-interface.js
@@ -3,18 +3,11 @@ var GhUser = require('./../js/ghUser.js').GhUser;
 
 $(document).ready(function(){
 
-  // $("#hubForm").submit(function(event){
-  //   event.preventDefault();
-  //   var username = $("#usernameInput").val();
-  // })
-
   $.get('https://api.github.com/users/daneden/repos?access_token=' + apiKey).then(function(response){
-
     response.forEach(function(repo){
-      console.log(repo.name)
-      console.log(repo.description)
+      $(".repoNameList").append("<li>" + repo.name + "</li>");
+      $(".repoDescList").append("<li>" + repo.description + "</li>");
     })
-
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
