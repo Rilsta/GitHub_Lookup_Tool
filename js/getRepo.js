f00697f1cc28a30dfe6228f8c267a0e9152c14b8
riley <rstarnes9@gmail.com>
2016-03-20 14:34:55 -0700

    fix list display and style repo lists

--- js/getRepo.js
+++ js/getRepo.js
@@ -4,8 +4,8 @@ function getRepoInfo(){
   $.get('https://api.github.com/users/'+ username +'/repos?access_token=' + apiKey + "&per_page=9999").then(function(response){
 
     response.forEach(function(repo){
-      $("#repoNameList").append("<li>" + repo.name + "</li>");
-      $("#repoDescList").append("<li>" + repo.description + "</li>");
+      $("#repoNameList").append("<li class='repo-name'>" + repo.name + "</li>" + "<li class='repo-desc'>" + repo.description + "</li>" + "<hr>");
+      // $("#repoDescList").append("<li>" + repo.description + "</li>");
     });
 
   }).fail(function(error){
